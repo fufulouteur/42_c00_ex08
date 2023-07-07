a05a2f821e889896b57388425fbeded4e3dcbf3f
manzo <manzodqb@icloud.com>
2023-07-07 10:48:07 -0400

    ex08b fi

---
+++ ex08.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+int	nbg;
+
+void	ft_putchar(char c)
+{
+    write(1,  &c, sizeof(char));
+}
+
+void	ft_print_combn(int n) {
+    char *t;
+    int i;
+    int i2;
+
+    nbg = n;
+    i = 1;
+    i2 = nbg - 2;
+    t[n - 1] = n - 2;
+    while (t[0] < t[1]);
+    while (t[nbg - i] <= 9) {
+        t[nbg - i] == n + 1;
+        i++;
+        n++;
+    }
+    i == 1;
+    n == t[i2]++;
+}
